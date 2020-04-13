b3fef9088c5597b6c49686e9bfdbc207c9b0a9b2
Ian Denhardt <ian@zenhack.net>
2020-04-13 03:21:46 -0400

    Add a comment

--- oflags.h
+++ oflags.h
@@ -6,5 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// Mask off everything but the access flags, so we can do comparisons
+// like OFLAG_ACCESS(flags) == O_RDONLY
 #define OFLAG_ACCESS(flags) \
 	((flags) & (O_RDONLY | O_RDWR | O_WRONLY))
